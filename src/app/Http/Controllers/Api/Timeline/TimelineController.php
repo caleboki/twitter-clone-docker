c820937fc30a17cbbd0bf8ffe56fdc60dc9d8cc6
Caleb Oki <caleb.oki@ibm.com>
2020-05-31 12:49:42 -0400

    feat: getting started on the timeline

---
+++ src/app/Http/Controllers/Api/Timeline/TimelineController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Timeline;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\TweetCollection;
+
+class TimelineController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tweets = $request->user()->tweetsFromFollowing()->paginate(5);
+        return new TweetCollection($tweets);
+    }
+}
