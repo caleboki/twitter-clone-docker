b749e8c085be88e44cac2ecb92537aa1ab1f4558
Caleb Oki <caleb.oki@ibm.com>
2020-06-02 00:08:18 -0400

    feat: infinite tweet scroll

--- src/resources/js/components/timeline/AppTimeline.vue
+++ src/resources/js/components/timeline/AppTimeline.vue
@@ -5,6 +5,14 @@
             :key="tweet.id"
             :tweet="tweet"
         />
+
+        <div
+            v-if="tweets.length"
+            v-observe-visibility="{
+                callback: handleScrollToBottomOfTimeline
+            }">
+
+        </div>
     </div>
 </template>
 
@@ -12,20 +20,51 @@
     import { mapGetters, mapActions } from 'vuex'
 
     export default {
+        data() {
+            return {
+                page: 1,
+                lastPage: 1
+            }
+        },
         computed: {
             ...mapGetters({
                 tweets: 'timeline/tweets'
-            })
+            }),
+
+            urlWithPage () {
+                return `/api/timeline?page=${this.page}`
+            }
         },
 
         methods: {
             ...mapActions({
                 getTweets: 'timeline/getTweets'
-            })
+            }),
+
+            loadTweets () {
+                this.getTweets(this.urlWithPage).then((response) => {
+                    this.lastPage = response.data.meta.last_page
+                })
+            },
+
+            handleScrollToBottomOfTimeline (isVisible) {
+                if (!isVisible) {
+                    return
+                }
+
+                if (this.lastPage === this.page) {
+                    return
+                }
+
+                this.page++
+                this.loadTweets()
+
+                console.log('bottom reached')
+            }
         },
 
         mounted() {
-            this.getTweets()
+            this.loadTweets()
         },
     }
 </script>
