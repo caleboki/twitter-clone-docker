594809ce8e12f709516d69bac01634ac78ef51a3
Caleb Oki <caleb.oki@ibm.com>
2020-07-22 01:14:49 -0400

    Setting up for likes

--- src/database/migrations/2020_07_22_042526_create_likes_table.php
+++ src/database/migrations/2020_07_22_042526_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTweetsTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateTweetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tweets', function (Blueprint $table) {
-            $table->id()->unsigned();
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->unsigned()->index();
-            $table->text('body')->nullable();
+            $table->foreignId('tweet_id')->unsigned()->index();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('tweet_id')->references('id')->on('tweets')->onDelete('cascade');
         });
     }
 
@@ -30,6 +31,6 @@ class CreateTweetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tweets');
+        Schema::dropIfExists('likes');
     }
 }
