febdf17a4d4fe4b08ae56d699a276d215f0c1866
Caleb Oki <caleb.oki@ibm.com>
2020-08-02 14:37:43 -0400

    Retweet dropdown menu

---
+++ src/resources/js/components/dropdown/AppDropdownItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <a href="#" class="block py-3 px-4 text-gray-300 hover:bg-gray-800" @click.prevent="handleClick">
+            <slot />
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleClick(e) {
+                this.$emit('click', e)
+                this.$parent.close()
+            }
+        },
+    }
+</script>
